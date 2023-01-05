4af4ff282b64af912ff6eb05b6b1d5f7128696ff
highz0r <bnmmnb1337@gmail.com>
2023-01-05 05:18:20 +0300

    TASK-1 -added table models;

---
+++ Web.Html.Parser.App/Data/ItemModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.Html.Parser.App.Data;
+public class ItemModel
+{
+    public Guid Id { get; set; }
+    public Guid SellerId { get; set; }
+    public Guid GameId { get; set; }
+    public string Description { get; set; }
+}
---
+++ Web.Html.Parser.App/Data/UserModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.Html.Parser.App.Data;
+public class UserModel
+{
+    public Guid Id { get; set; }
+    public int WebId { get; set; }
+    public string UserName { get; set; }
+}
